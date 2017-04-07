8f7ddb113398b1b357eb71dd2fcefe163575b1a4
ArlenPP <htoatot@gmail.com>
2017-04-07 11:10:32 +0800

    add_file_29

---
+++ 29/run.sh
@@ -0,0 +1,7 @@
+python train_and_test.py
+cd ../../../output/29
+svm-scale -s train-data-scale-info train-data > train-data.scale
+svm-train -b 1 train-data.scale train-model
+svm-scale -r train-data-scale-info test-data > test-data.scale
+svm-predict -b 1 test-data.scale train-model test-result
+
