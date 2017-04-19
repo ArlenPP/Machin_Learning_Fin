277acf58e149b1196d590d418291db3be2ff726a
ArlenPP <htoatot@gmail.com>
2017-04-19 12:56:41 +0800

    debug_py_of_train_test

--- 10/roi.py
+++ 10/roi.py
@@ -26,44 +26,81 @@ for row in csv.DictReader(test):
 	close.append(float(row['Close']))
 
 buymoney=0
+sellmoney=0
 savemoney=0
+profit=0
 x=0
-#print(open[len(open)-len(labels)-29])
+
+
 for i in range (0,len(labels),1):
 
+	if(profit>0):
+		buymoney=sellmoney-profit+profit*3/4
+		savemoney=savemoney+profit/4
+
 	
-	buymoney=savemoney*3/4
-	savemoney=savemoney/4
-	#print(open[i+10])
-	#print(close[i+39])
-	if(labels[i]==1):
-		if(buymoney<open[i+10]):
-			initmoney=initmoney-(open[i+10]-buymoney)
-			buymoney=open[i+10]   #=buymoney+(open[i]-buymoney) 
+		if(labels[i]==1):
+			if(buymoney<open[i+10]):
+				initmoney=initmoney-(open[i+10]-buymoney)
+				buymoney=open[i+10]   #=buymoney+(open[i]-buymoney) 
+			number=1
+			#number=math.floor(buymoney/open[i+10])
+			sellmoney=number*close[i+40]
+			profit=number*(close[i+40]-open[i+10])
+			x=x+profit
+
+			#y=y+profit/open[i+10]
+			#here will have problem because the money actualy return at 30 days in the future
+
+		elif(labels[i]==-1):
+
+			if(buymoney<open[i+10]):
+				initmoney=initmoney-(open[i+10]-buymoney)
+				buymoney=open[i+10]   #=buymoney+(open[i]-buymoney) 
+			
+			number=1
+			#number=math.floor(buymoney/open[i+10])
+			sellmoney=number*close[i+40]
+			profit = number*(open[i+10]-close[i+40])
+			x=x+profit
 		
-		number=1
-		buymoney=buymoney-number*open[i]
-		sellmoney=number*close[i+40]
-		x=x+close[i+40]-open[i+10]
+	else:
+		buymoney=sellmoney
+		if(labels[i]==1):
+			if(buymoney<open[i+10]):
+				initmoney=initmoney-(open[i+10]-buymoney)
+				buymoney=open[i+10]   #=buymoney+(open[i]-buymoney) 
+			
+			number=1
+			#number=math.floor(buymoney/open[i+10])
+			sellmoney=number*close[i+40]
+			profit=number*(close[i+40]-open[i+10])
+			x=x+profit
+			
+			#x=x+close[i+40]-open[i+10]
 
-		#here will have problem because the money actualy return at 30 days in the future
+			#here will have problem because the money actualy return at 30 days in the future
 
-	elif(labels[i]==-1):
+		elif(labels[i]==-1):
 
-		if(buymoney<open[i+10]):
-			initmoney=initmoney-(open[i+10]-buymoney)
-			buymoney=open[i+10]   #=buymoney+(open[i]-buymoney) 
-		
-		number=1
-		buymoney=buymoney-number*close[i+39]
-		sellmoney=number*open[i+10]
-		x=x+open[i+10]-close[i+39]
-	savemoney=savemoney+buymoney+sellmoney
-	'''
+			if(buymoney<open[i+10]):
+				initmoney=initmoney-(open[i+10]-buymoney)
+				buymoney=open[i+10]   #=buymoney+(open[i]-buymoney) 
+			
+			number=1
+			#number=math.floor(buymoney/open[i+10])
+			sellmoney=number*open[i+10]
+			profit = number*(open[i+10]-close[i+40])
+			x=x+profit
+	print(open[i+10],close[i+40],x)
+	#print(x)
+		#x=x+open[i+10]-close[i+40]
+finialmoney=sellmoney+profit+sellmoney
+'''
 	buymoney=savemoney*3/4
 	savemoney=savemoney/4
 	print(open[i+10])
-	#print(close[i+39])
+	#print(close[i+40])
 	if(labels[i]==1):
 		if(buymoney<open[i+10]):
 			initmoney=initmoney-(open[i+10]-buymoney)
@@ -71,8 +108,8 @@ for i in range (0,len(labels),1):
 		
 		number=math.floor(buymoney/open[i])
 		buymoney=buymoney-number*open[i]
-		sellmoney=number*close[i+39]
-		x=x+close[i+39]-open[i]
+		sellmoney=number*close[i+40]
+		x=x+close[i+40]-open[i]
 
 		#here will have problem because the money actualy return at 30 days in the future
 
@@ -83,15 +120,14 @@ for i in range (0,len(labels),1):
 			buymoney=open[i+10]   #=buymoney+(open[i]-buymoney) 
 		
 		number=math.floor(buymoney/open[i+10])
-		buymoney=buymoney-number*close[i+39]
+		buymoney=buymoney-number*close[i+40]
 		sellmoney=number*open[i+10]
-		x=x+open[i+10]-close[i+39]
+		x=x+open[i+10]-close[i+40]
 	savemoney=savemoney+buymoney+sellmoney
 	
 '''
-finialmoney=savemoney
-print(savemoney)
 initmoney=-initmoney
-print(initmoney)
 print(finialmoney/initmoney)
 print(x)
+
+
