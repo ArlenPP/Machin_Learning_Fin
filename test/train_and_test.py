0c821698aee2293271e847f97300eb1a24ce3a6f
ArlenPP <htoatot@gmail.com>
2017-05-10 10:22:46 +0800

    cost_2010

--- test/train_and_test.py
+++ test/train_and_test.py
@@ -62,9 +62,9 @@ for day in range(5,len(Open),1):
 		a='0'
 	if(a=='1' or a=='-1'):
 		if(a=='1'):
-			control = 28
+			control = 20
 		elif(a=='-1'):
-			control = 17
+			control = 14
 		while control > 0:
 			control -= 1
 			file.write(a)
