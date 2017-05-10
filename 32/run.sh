37f4d6018805019f48bf69cdd8eff61fc1909af9
ArlenPP <htoatot@gmail.com>
2017-05-10 09:30:38 +0800

    change

--- 32/run.sh
+++ 32/run.sh
@@ -1,5 +1,5 @@
 python train_and_test.py
-cd ../../../output/32
+cd ../../../output/test
 svm-scale -s train-data-scale-info train-data > train-data.scale
 svm-train -b 1 train-data.scale train-model
 svm-scale -r train-data-scale-info test-data > test-data.scale
