7c9379420b7c96d93e72e52fd5219a390c2460ba
ArlenPP <htoatot@gmail.com>
2017-05-24 09:19:47 +0800

    file 52 run change

--- 52/run.sh
+++ 52/run.sh
@@ -1,5 +1,5 @@
 python train_and_test.py
-cd ../../../output/50
+cd ../../../output/52
 svm-scale -s train-data-scale-info train-data > train-data.scale
 svm-train -b 1 train-data.scale train-model
 svm-scale -r train-data-scale-info test-data > test-data.scale
