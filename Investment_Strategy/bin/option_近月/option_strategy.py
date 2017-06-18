67300d1897a16afc6634d184067b15fadc8667cf
ArlenPP <htoatot@gmail.com>
2017-06-18 15:58:59 +0800

    stock_TrainTestMaker finish and ROI UPdate

---
+++ Investment_Strategy/bin/option_近月/option_strategy.py
@@ -0,0 +1,215 @@
+#====   for typing chinese  ====#
+#!/usr/bin/python
+# coding=utf8
+
+import sys
+import os
+import csv
+import math
+import datetime
+import codecs
+import re
+from dateutil.relativedelta import relativedelta
+from option_class import porfolio
+from option_class import transation
+from option_class import test_result
+from option_class import txff
+
+##      set up porfolio condition       ##
+
+transation_Time = 1
+train_data_feature_few_day = 10
+allin = 1
+save = 0
+price_out = 1
+##====	choose do Call or Put 1 is do 0 is not do 		====##
+DoCall = 1
+DoPut = 0
+
+
+##      define function     ##
+def CallorPutPrice(open_price,label):
+	if(label == 1):
+		if(open_price>=10000):
+			x = open_price%200
+			if(x>=100):
+				y = open_price + (200-x) + (200*price_out)
+				return y
+			else:
+				y = open_price - x + (200*price_out)
+				return y
+		else:
+			x=open_price%100
+			if(x>=50):
+				y=open_price+(100-x) + (100*price_out)
+				return y
+			else:
+				y=open_price-x + (100*price_out)
+				return y
+	elif(label == -1):
+		if(open_price>=10000):
+			x = open_price%200
+			if(x>=100):
+				y = open_price + (200-x) - (200*price_out)
+				return y
+			else:
+				y = open_price - x - (200*price_out)
+				return y
+		else:
+			x=open_price%100
+			if(x>=50):
+				y=open_price+(100-x) - (100*price_out)
+				return y
+			else:
+				y=open_price-x - (100*price_out)
+				return y
+def ROI():
+	tmp = 0
+	for i in iter(my_porfolio.transation_list):
+		if(i.haveorno == 1):
+			tmp += i.number*i.buyprice*50
+	roi = (tmp + my_porfolio.money_canbuy + my_porfolio.output_money + my_porfolio.money_deposit)/my_porfolio.total_output_money
+	return roi
+
+def TheEed():
+	##====		!!!!problem!!!! 	====##	
+	print ROI()
+	sys.exit()
+
+##      start code here         ##
+
+my_porfolio = porfolio(transation_Time)
+my_test_result = test_result()
+my_txff = txff(train_data_feature_few_day)
+f=open('../../option_roi_result.csv','w')
+f.write("date,roi\n")
+
+buyfilelist = open('../../buyfilelist.csv','w')
+buyfilelist.write("buy_or_sell,day,strike_price,number,id,PutorCall,buyprice,output_money,money_canbuy\n")
+
+sellfilelist = open('../../sellfilelist.csv','w')
+sellfilelist.write("buy_or_sell,sellday,id,number,buyday,buyprice,sellprice\n")
+
+my_option = list(csv.DictReader(open('../../totaloption.csv','r')))
+
+##====      start roi       ====##
+which = 0
+for i in range(0,len(my_test_result.label),1):
+	##====      to count how many days      ====#
+	for tra in iter(my_porfolio.transation_list):
+		if(tra.haveorno == 1):
+			tra.howmanyday += 1
+	##====      if label != 0       ====##
+	if(my_test_result.label[i] != 0):
+		
+		# strike_price is the price of call or put #
+		strike_price = CallorPutPrice(my_txff.open[i],my_test_result.label[i])
+
+		#   determine the dateline of option we buy     #
+		date_after_month = datetime.datetime.strptime(my_txff.date[i], '%Y/%m/%d') + relativedelta(months=1)
+		
+		#  if 10 day is comming  start find the price in my_option   #
+		for tra in iter(my_porfolio.transation_list):
+			if(tra.howmanyday == transation_Time and tra.haveorno == 1 and DoCall == 1):
+				for row in my_option:
+					if(my_txff.date[i]==row['date'] and tra.strike_price == float(row['strike_price']) and tra.callorput == row['callorput'] and tra.dateline == row['dateline']):
+						if(tra.Take_Profit(float(row['high']))):
+							my_porfolio.money_deposit += tra.number*(tra.sellprice-0.5)*save*50
+							my_porfolio.money_canbuy += tra.number*(tra.sellprice-0.5)*allin*50
+							tra.init_sell(row['date'])
+							sellfilelist.write('sell'+','+str(tra.sellday)+','+str(tra.id)+','+str(tra.number)+','+str(tra.buyday)+','+str(tra.buyprice)+','+str(tra.sellprice)+'\n')
+							break
+						elif(tra.Stop_Loss(float(row['low']))):
+							my_porfolio.money_canbuy += tra.number*(tra.sellprice-0.5)*50
+							tra.init_sell(row['date'])
+							sellfilelist.write('sell'+','+str(tra.sellday)+','+str(tra.id)+','+str(tra.number)+','+str(tra.buyday)+','+str(tra.buyprice)+','+str(tra.sellprice)+'\n')
+							break
+						my_porfolio.money_canbuy += tra.number*(float(row['close'])-0.5)*50
+						tra.sellprice = float(row['close'])
+						tra.init_sell(row['date'])
+						sellfilelist.write('sell'+','+str(tra.sellday)+','+str(tra.id)+','+str(tra.number)+','+str(tra.buyday)+','+str(tra.buyprice)+','+str(tra.sellprice)+'\n')
+						break
+
+
+		for row in my_option:
+			if(strike_price == float(row['strike_price']) and my_txff.date[i] == row['date'] and date_after_month.strftime("%Y%m")==row['dateline']):
+				if(my_test_result.label[i] == 1 and row['callorput']=='call'):	
+					if(my_porfolio.output_money <= 0):
+						TheEed()				
+					else:
+						##====      get money from out_put to canbuy        ====##
+						today_buyprice = float(row['open'])
+						if(my_porfolio.money_canbuy <= (today_buyprice + 0.5)*50):
+							my_porfolio.output_money -= ((today_buyprice + 0.5)*50 - my_porfolio.money_canbuy)
+							my_porfolio.money_canbuy = (today_buyprice + 0.5)*50
+						
+						which = my_porfolio.WhichTransation()
+
+						my_porfolio.transation_list[which].buy(today_buyprice,my_porfolio.money_canbuy,row['date'],float(row['strike_price']),row['callorput'],row['dateline'],my_test_result.call[i])
+						my_porfolio.money_canbuy -= my_porfolio.transation_list[which].number*((today_buyprice+0.5)*50)
+						print ('buy','transation_day = '+str(row['date']),'strike_price = '+str(row['strike_price']),'number = '+str(my_porfolio.transation_list[which].number),'id = '+str(my_porfolio.transation_list[which].id),'PutCall = '+str(row['callorput']),'buyprice = '+str(today_buyprice),'output_money = '+str(my_porfolio.output_money),'money_canbuy = '+str(my_porfolio.money_canbuy))
+						buyfilelist.write('buy'+','+str(row['date'])+','+str(row['strike_price'])+','+str(my_porfolio.transation_list[which].number)+','+str(my_porfolio.transation_list[which].id)+','+str(row['callorput'])+','+str(today_buyprice)+','+str(my_porfolio.output_money)+','+str(my_porfolio.money_canbuy)+'\n')
+						break
+						
+				elif(my_test_result.label[i] == -1 and row['callorput']=='put' and DoPut == 1):
+					if(my_porfolio.output_money <= 0):
+						TheEed()
+						
+					else:
+						##====      get money from out_put to canbuy        ====##
+						today_buyprice = float(row['open'])
+						if(my_porfolio.money_canbuy <= ((today_buyprice+0.5)*50)):
+							my_porfolio.output_money -= (((today_buyprice+0.5)*50) - my_porfolio.money_canbuy)
+							my_porfolio.money_canbuy = ((today_buyprice+0.5)*50)
+						
+						which = my_porfolio.WhichTransation()
+
+						my_porfolio.transation_list[which].buy(row['open'],my_porfolio.money_canbuy,row['date'],float(row['strike_price']),row['callorput'],row['dateline'],my_test_result.put[i])
+						my_porfolio.money_canbuy -= my_porfolio.transation_list[which].number*((float(row['open'])+0.5)*50)
+						print ('buy','transation_day = '+str(row['date']),'strike_price = '+str(row['strike_price']),'number = '+str(my_porfolio.transation_list[which].number),'id = '+str(my_porfolio.transation_list[which].id),'PutCall = '+str(row['callorput']),'buyprice = '+str(today_buyprice),'output_money = '+str(my_porfolio.output_money),'money_canbuy = '+str(my_porfolio.money_canbuy))
+						buyfilelist.write('buy'+','+str(row['date'])+','+str(row['strike_price'])+','+str(my_porfolio.transation_list[which].number)+','+str(my_porfolio.transation_list[which].id)+','+str(row['callorput'])+','+str(today_buyprice)+','+str(my_porfolio.output_money)+','+str(my_porfolio.money_canbuy)+'\n')
+						break
+				# edit
+		for tra in iter(my_porfolio.transation_list):
+			if(tra.haveorno == 1):
+				for row in my_option:
+					if(my_txff.date[i]==row['date'] and tra.strike_price == float(row['strike_price']) and tra.callorput == row['callorput'] and tra.dateline == row['dateline']):
+						if(tra.Take_Profit(float(row['high']))):
+							my_porfolio.money_deposit += tra.number*(tra.sellprice-0.5)*save*50
+							my_porfolio.money_canbuy += tra.number*(tra.sellprice-0.5)*allin*50
+							tra.init_sell(row['date'])
+							sellfilelist.write('sell'+','+str(tra.sellday)+','+str(tra.id)+','+str(tra.number)+','+str(tra.buyday)+','+str(tra.buyprice)+','+str(tra.sellprice)+'\n')
+						elif(tra.Stop_Loss(float(row['low']))):
+							my_porfolio.money_canbuy += tra.number*(tra.sellprice-0.5)*50
+							tra.init_sell(row['date'])
+							sellfilelist.write('sell'+','+str(tra.sellday)+','+str(tra.id)+','+str(tra.number)+','+str(tra.buyday)+','+str(tra.buyprice)+','+str(tra.sellprice)+'\n')
+						break
+
+
+	# if labels == 0
+	else:
+		#you need to check is the price ok
+		for tra in iter(my_porfolio.transation_list):
+			if(tra.haveorno == 1):
+				for row in my_option:
+					if(my_txff.date[i]==row['date'] and tra.strike_price == float(row['strike_price']) and tra.callorput == row['callorput'] and tra.dateline == row['dateline']):
+						if(tra.Take_Profit(float(row['high']))):
+							my_porfolio.money_deposit += tra.number*(tra.sellprice-0.5)*save*50
+							my_porfolio.money_canbuy += tra.number*(tra.sellprice-0.5)*allin*50
+							tra.init_sell(row['date'])
+							sellfilelist.write('sell'+','+str(tra.sellday)+','+str(tra.id)+','+str(tra.number)+','+str(tra.buyday)+','+str(tra.buyprice)+','+str(tra.sellprice)+'\n')
+						elif(tra.Stop_Loss(float(row['low']))):
+							my_porfolio.money_canbuy += tra.number*(tra.sellprice-0.5)*50
+							tra.init_sell(row['date'])
+							sellfilelist.write('sell'+','+str(tra.sellday)+','+str(tra.id)+','+str(tra.number)+','+str(tra.buyday)+','+str(tra.buyprice)+','+str(tra.sellprice)+'\n')
+						elif(tra.howmanyday == transation_Time):
+							my_porfolio.money_canbuy += tra.number*(float(row['close'])-0.5)*50
+							tra.sellprice = float(row['close'])
+							tra.init_sell(row['date'])
+							sellfilelist.write('sell'+','+str(tra.sellday)+','+str(tra.id)+','+str(tra.number)+','+str(tra.buyday)+','+str(tra.buyprice)+','+str(tra.sellprice)+'\n')
+						break
+	##==== start write the roi file  ====##
+	f.write(str(my_txff.date[i])+','+str(ROI())+'\n')
+	print (ROI())
+
+TheEed()	
--- Make_Train_Test_Data/stock_TrainTest.py
+++ Make_Train_Test_Data/stock_TrainTest.py
@@ -1,12 +1,126 @@
-
+import os
+import csv
+import math
+import datetime
+import codecs
+import re
+from dateutil.relativedelta import relativedelta
+from stock_class import stock
+from stock_class import stock_combination
+from stock_class import FeatureWrite
 
 
 ##====    set up config 	====##
 
-train_data_startday = 
-train_data_endday = 
-test_data_startday = 
-test_data_endday = 
+train_data_startday = '2002/1/2'
+train_data_endday = '2011/12/30'
+test_data_startday = '2012/1/2'
+test_data_endday = '2012/12/28'
 
-transation_Time = 10
+over_x_is_high = 100
+over_y_is_low =  100
+transation_Time = 1
 train_data_feature_few_day = 10
+on_arlen_computer = 0
+file = '53'
+##====		set up where is history option file 		====##
+dir_of_stock = './tmp_database/'
+
+
+##==== set up file dir 		====##
+if(on_arlen_computer == 1):
+	train = open('./train-data','w')
+	test = open('./test-data','w')
+else:
+	train = open('../../../output/'+file+'/train-data','w')
+	test = open('../../../output/'+file+'/test-data','w')
+
+
+##====		START code 		====##
+##====		read stock file 		====##
+
+filelist_stock = [] #list of tmp_option.csv filename
+
+for filename in os.listdir(dir_of_stock):
+	filelist_stock.append(filename)
+
+#====		good initial sort but doesnt sort numerically very well so we need to use key: and f:
+def natural_key(string_):
+	return [int(s) if s.isdigit() else s for s in re.split(r'(\d+)', string_)]
+
+filelist_stock.sort(key=natural_key)
+
+##==== 		open stock file 	====##
+
+my_stock_combination = stock_combination(filelist_stock)
+
+##==== 		append data into my_stock 	====##
+for my_stock in my_stock_combination.stock_list:
+	tmp = open(dir_of_stock+my_stock.stockname,'r')
+	my_stock.AppendData(tmp)
+
+##==== 		start to make train and test 	 ====##
+for my_stock in my_stock_combination.stock_list:
+	
+	##==== 	Enter Train Test START and End Day ====##
+	for i in range(len(my_stock.Open)):
+		if(my_stock.Date[i] == train_data_startday):
+			my_stock.TrainStarti = i
+		if(my_stock.Date[i] == train_data_endday):
+			my_stock.TrainEndi = i
+		if(my_stock.Date[i] == test_data_startday):
+			my_stock.TestStarti = i
+		if(my_stock.Date[i] == test_data_endday):
+			my_stock.TestEndi = i
+	print (my_stock.TrainStarti,my_stock.TestEndi)
+	print my_stock.stockname
+##====		make train 		====##
+## range(i_of_startday,i_of_endday,+=1 each time)
+number = []
+number.append(int(1))
+for i in range(0,(my_stock_combination.stock_list[0].TrainEndi - my_stock_combination.stock_list[0].TrainStarti)+1,1):
+	number[0] = 1
+	for my_stock in my_stock_combination.stock_list:
+		
+		if(my_stock.stockname == '1txff.csv'):
+			HighOrLowHappened = 0
+			for x in range(transation_Time):
+				if(my_stock.High[my_stock.TrainStarti+i+x] > my_stock.Open[my_stock.TrainStarti+i]+over_x_is_high):
+					train.write('1')
+					HighOrLowHappened = 1
+					break
+				elif(my_stock.Low[my_stock.TrainStarti+i+x] < my_stock.Open[my_stock.TrainStarti+i]-over_y_is_low):
+					train.write('-1')
+					HighOrLowHappened = 1
+					break
+			if(HighOrLowHappened == 0):
+				train.write('0')
+		
+		##==== con if is train == 1 if is test == 0 	====##
+		con = 1
+		FeatureWrite(train,train_data_feature_few_day,my_stock,i,number,con)
+	train.write('\n')
+
+##		make test 		##
+
+for i in range(0,(my_stock_combination.stock_list[0].TestEndi - my_stock_combination.stock_list[0].TestStarti)+1,1):
+	number[0] = 1
+	for my_stock in my_stock_combination.stock_list:
+		
+		if(my_stock.stockname == '1txff.csv'):
+			HighOrLowHappened = 0
+			for x in range(transation_Time):
+				if(my_stock.High[my_stock.TestStarti+i+x] > my_stock.Open[my_stock.TestStarti+i]+over_x_is_high):
+					test.write('1')
+					HighOrLowHappened = 1
+					break
+				elif(my_stock.Low[my_stock.TestStarti+i+x] < my_stock.Open[my_stock.TestStarti+i]-over_y_is_low):
+					test.write('-1')
+					HighOrLowHappened = 1
+					break
+			if(HighOrLowHappened == 0):
+				test.write('0')
+		##==== con if is train == 1 if is test == 0 	====##
+		con = 0
+		FeatureWrite(test,train_data_feature_few_day,my_stock,i,number,con)
+	test.write('\n')
